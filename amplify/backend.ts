c712a6e9da22da32c38e7974153a118af2064055
Jon Wire <wirej@amazon.com>
2025-04-10 15:23:00 -0500

    failing state, one of the issues customers are seeing

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,15 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { aws_events } from "aws-cdk-lib";
+import {
+  Effect,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -8,4 +17,87 @@ import { data } from './data/resource';
 const backend = defineBackend({
   auth,
   data,
+});
+
+// Create a new stack for the EventBridge data source
+const eventStack = backend.createStack("MyExternalDataSources");
+
+// Reference or create an EventBridge EventBus
+const eventBus = aws_events.EventBus.fromEventBusName(
+  eventStack,
+  "MyEventBus",
+  "default"
+);
+
+backend.data.addEventBridgeDataSource("MyEventBridgeDataSource", eventBus);
+
+// Create a policy statement to allow invoking the AppSync API's mutations
+const policyStatement = new PolicyStatement({
+  effect: Effect.ALLOW,
+  actions: ["appsync:GraphQL"],
+  resources: [`${backend.data.resources.graphqlApi.arn}/types/Mutation/*`],
+});
+
+// Create a role for the EventBus to assume
+const eventBusRole = new Role(eventStack, "AppSyncInvokeRole", {
+  assumedBy: new ServicePrincipal("events.amazonaws.com"),
+  inlinePolicies: {
+    PolicyStatement: new PolicyDocument({
+      statements: [policyStatement],
+    }),
+  },
+});
+
+// Create an EventBridge rule to route events to the AppSync API
+const rule = new aws_events.CfnRule(eventStack, "MyOrderRule", {
+  eventBusName: eventBus.eventBusName,
+  name: "broadcastOrderStatusChange",
+  eventPattern: {
+    source: ["amplify.orders"],
+    /* The shape of the event pattern must match EventBridge's event message structure.
+    So, this field must be spelled as "detail-type". Otherwise, events will not trigger the rule.
+
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/ev-events.html
+    */
+    ["detail-type"]: ["OrderStatusChange"],
+    detail: {
+      orderId: [{ exists: true }],
+      status: ["PENDING", "SHIPPED", "DELIVERED"],
+      message: [{ exists: true }],
+    },
+  },
+  targets: [
+    {
+      id: "orderStatusChangeReceiver",
+      arn: backend.data.resources.cfnResources.cfnGraphqlApi
+        .attrGraphQlEndpointArn,
+      roleArn: eventBusRole.roleArn,
+      appSyncParameters: {
+        graphQlOperation: `
+        mutation PublishOrderFromEventBridge(
+          $orderId: String!
+          $status: String!
+          $message: String!
+        ) {
+          publishOrderFromEventBridge(orderId: $orderId, status: $status, message: $message) {
+            orderId
+            status
+            message
+          }
+        }`,
+      },
+      inputTransformer: {
+        inputPathsMap: {
+          orderId: "$.detail.orderId",
+          status: "$.detail.status",
+          message: "$.detail.message",
+        },
+        inputTemplate: JSON.stringify({
+          orderId: "<orderId>",
+          status: "<status>",
+          message: "<message>",
+        }),
+      },
+    },
+  ],
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -28,8 +28,56 @@ const schema = a
     .returns(a.customType({
       summary: a.string()
     }))
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    // for testing event bridge
+    OrderStatus: a.enum(["OrderPending", "OrderShipped", "OrderDelivered"]),
+    OrderStatusChange: a.customType({
+      orderId: a.id().required(),
+      status: a.ref("OrderStatus").required(),
+      message: a.string().required(),
+    }),
+
+      
+  publishOrderToEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
     .authorization((allow) => [allow.publicApiKey()])
-  })
+    .handler(
+      a.handler.custom({
+        dataSource: "EventBridgeDataSource",
+        entry: "./publishOrderToEventBridge.js",
+      })
+    ),
+  publishOrderFromEventBridge: a
+    .mutation()
+    .arguments({
+      orderId: a.id().required(),
+      status: a.string().required(),
+      message: a.string().required(),
+    })
+    .returns(a.ref("OrderStatusChange"))
+    .authorization((allow) => [allow.publicApiKey(), allow.guest()])
+    .handler(
+      a.handler.custom({
+        entry: "./publishOrderFromEventBridge.js",
+      })
+    ),
+  onOrderFromEventBridge: a
+    .subscription()
+    .for(a.ref("publishOrderFromEventBridge"))
+    .authorization((allow) => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        entry: "./onOrderFromEventBridge.js",
+      })
+    ),
+    })
   .authorization((allow) => [allow.publicApiKey()]);
 
 export type Schema = ClientSchema<typeof schema>;
