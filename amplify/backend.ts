2c98cc37694255e473c840998f8e070abcf2f465
Jon Wire <iambipedal@gmail.com>
2025-05-12 14:01:05 -0500

    bump

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -51,7 +51,7 @@ const eventBusRole = new Role(eventStack, "AppSyncInvokeRole", {
 });
 
 // Create an EventBridge rule to route events to the AppSync API
-const rule = new aws_events.CfnRule(eventStack, "MyOrderRule2", {
+const rule = new aws_events.CfnRule(eventStack, "MyOrderRule", {
   eventBusName: eventBus.eventBusName,
   name: "broadcastOrderStatusChange",
   eventPattern: {
